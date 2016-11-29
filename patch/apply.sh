7d9ce3028854a620df978d4ab217eac50afc8436
Syl191 <slayeurxdu91@gmail.com>
2016-11-29 23:31:12 +0100

    Add HWC+Patch, Fixs by Sudru

--- patch/apply.sh
+++ patch/apply.sh
@@ -1,3 +1,4 @@
 cd ../../../../frameworks/native/services/surfaceflinger
 git apply -v ../../../../device/huawei/frd/patch/0000-fix-deep-sleep-if-not-hwc.patch
-
+cd ../../../../hardware/libhardware/modules/gralloc/
+git apply -v ../../../../device/huawei/frd/patch/0001-fix-hwc-build.patch
